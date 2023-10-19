f2604760e17ee4eb8823903f6fe403a3dc4053df
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-10-19 07:07:26 +0200

    added resetCounter function and confirmation code

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const STEP_AMOUNT = 5;
 const number = document.querySelector('[data-key="number"]');
 const subtract = document.querySelector('[data-key="subtract"]');
 const add = document.querySelector('[data-key="add"]');
+const resetButton = document.querySelector('[data-key="reset"]');
+const confirmationMessage = document.getElementById('confirmation-message');
+
 
 const subtractHandler = () => {
   const newValue = parseInt(number.value) - STEP_AMOUNT;
@@ -32,5 +35,16 @@ const addHandler = () => {
   }
 };
 
+const resetCounter = () => {
+  number.value = MIN_NUMBER;
+  subtract.disabled = true;
+  add.disabled = false;
+  confirmationMessage.style.display = 'block';
+  setTimeout(() => {
+    confirmationMessage.style.display = 'none';
+  }, 2000);
+};
+
 subtract.addEventListener("click", subtractHandler);
 add.addEventListener("click", addHandler);
+resetButton.addEventListener("click", resetCounter);
